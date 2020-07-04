f1b3bcbe48276f9535a77a669fb3b355c752f6ed
LoiND <danhloi2210@gmail.com>
2020-07-04 10:09:09 +0700

    add script

---
+++ centos_optimize.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+echo 'net.ipv4.ip_local_port_range = 1024 65535' | sudo tee -a /etc/sysctl.conf
+echo 'fs.file-max = 5000000' | sudo tee -a /etc/sysctl.conf
+echo 'net.core.somaxconn = 500' | sudo tee -a /etc/sysctl.conf
+echo '* soft nproc 65535' | sudo tee -a /etc/security/limits.conf
+echo '* hard nproc 65535' | sudo tee -a /etc/security/limits.conf
+echo '* soft nofile 65535' | sudo tee -a /etc/security/limits.conf
+echo '* hard nofile 65535' | sudo tee -a /etc/security/limits.conf
