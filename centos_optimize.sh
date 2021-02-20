f93ba6ec16c9f38e70a94c22d47efc97f4331518
loind <danhloi2210@gmail.com>
2021-02-21 02:12:13 +0700

    update number config

--- centos_optimize.sh
+++ centos_optimize.sh
@@ -3,7 +3,7 @@
 echo 'net.ipv4.ip_local_port_range = 1024 65535' | sudo tee -a /etc/sysctl.conf
 echo 'fs.file-max = 5000000' | sudo tee -a /etc/sysctl.conf
 echo 'net.core.somaxconn = 500' | sudo tee -a /etc/sysctl.conf
-echo '* soft nproc 5000000' | sudo tee -a /etc/security/limits.conf
-echo '* hard nproc 5000000' | sudo tee -a /etc/security/limits.conf
-echo '* soft nofile 5000000' | sudo tee -a /etc/security/limits.conf
-echo '* hard nofile 5000000' | sudo tee -a /etc/security/limits.conf
+echo '* soft nproc 65535' | sudo tee -a /etc/security/limits.conf
+echo '* hard nproc 65535' | sudo tee -a /etc/security/limits.conf
+echo '* soft nofile 65535' | sudo tee -a /etc/security/limits.conf
+echo '* hard nofile 65535' | sudo tee -a /etc/security/limits.conf
